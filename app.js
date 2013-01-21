3950f247b8d5e87251b49af737f59e1c4fa7ccfd
hh54188 <hh54188@yahoo.cn>
2013-01-21 11:15:15 +0800

    complete

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+	,io = require('socket.io'); 
+
+var app = express()
+  , server = require('http').createServer(app)
+  , io = io.listen(server);
+  
+  server.listen(8000);
+
+io.sockets.on('connection', function (socket) {
+  // trigger the client
+  console.log('--->have client!');
+  console.log('--->trigger news event!');
+  socket.emit('news', { hello: 'world' });
+
+  // recevied the event
+  socket.on('client_event', function (data) {
+    console.log('---> trigger by the event', data);
+  });
+});
 
 //boot
 app.configure(function () {
 	app.use(express.static(__dirname + "/media"));
 	app.use(express.bodyParser());
+	app.set('view engine', 'ejs');
+	app.set('views', __dirname + '/views');
 })
 
 
 //router
 app.get('/', function (req, res) {
-	res.send("hello world");
+	res.render('client');
 });
-
-
-app.listen(8000);
 
